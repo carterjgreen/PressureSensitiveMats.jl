e540c5b405b1798bb7ad1b8bcf99442722af175e
Carter Green <carter.green@icloud.com>
2021-06-02 10:09:08 -0400

    add InvertedIndices

--- src/PressureSensitiveMats.jl
+++ src/PressureSensitiveMats.jl
@@ -1,6 +1,6 @@
 module PressureSensitiveMats
 
-using Distances, DSP, FFTW, Peaks, StatsBase, Statistics
+using Distances, DSP, FFTW, InvertedIndices, Peaks, StatsBase, Statistics
 
 export est_br, est_br_fft, est_br_fft2
 export PCC, PCC2, SNR_MAX, EGC, MRC_PSD, get_weights, combiner
