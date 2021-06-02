a5ae0c4f2d2187441aeb769c48262083b463a33c
Carter Green <carter.green@icloud.com>
2021-06-02 10:24:45 -0400

    export occupancy

--- src/PressureSensitiveMats.jl
+++ src/PressureSensitiveMats.jl
@@ -5,6 +5,7 @@ using Distances, DSP, FFTW, InvertedIndices, Peaks, StatsBase, Statistics
 export est_br, est_br_fft, est_br_fft2
 export PCC, PCC2, SNR_MAX, EGC, MRC_PSD, get_weights, combiner
 export Holtz, Solei, move_detect
+export breath_availability, occupancy_detection
 export active_sensors,
        apply2seg,
        choose_ref,
