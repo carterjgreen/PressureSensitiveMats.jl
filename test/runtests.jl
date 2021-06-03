eb43243e824b919b51e20cd80c33910f3d0ecd0c
Carter Green <carter.green@icloud.com>
2021-06-03 10:50:04 -0400

    add tests and occupancy for a single vector

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,22 @@ using Test
 
 @testset "PressureSensitiveMats.jl" begin
     @test sfm(zeros(512)) isa Number
+
+end
+
+@testset "Combiners" begin
+    multi = fill(313.0, (35000, 72)) .+ randn((35000, 72))
+    @test combiner(multi) isa AbstractVector
+end
+
+@testset "Occupancy Detection" begin
+    @test occupancy_detection(fill(313.0, (35000, 72)) .+ randn((35000, 72))) == zeros(35000)
+end
+
+@testset "Motion Detection" begin
+    single = fill(313.0, 35000) .+ randn(35000)
+    multi = fill(313.0, (35000, 72)) .+ randn((35000, 72))
+    @test move_detect(single) == zeros(35000)
+    @test move_detect(multi) == zeros(35000)
+    @test move_detect(single, Solei(), height=50, weight=50) == zeros(35000)
 end
